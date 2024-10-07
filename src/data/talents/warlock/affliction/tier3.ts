a2eefb01c61dbaa3169d71a24d016f054e26f411
Djimovanberlo <djimovanberlo@gmail.com>
2024-10-07 22:03:10 +0200

    changes

--- src/data/talents/warlock/affliction/tier3.ts
+++ src/data/talents/warlock/affliction/tier3.ts
@@ -1,10 +1,14 @@
-import { Talent } from '../../Classes'
+import { Talent } from "../../Classes";
 
 const tier3 = [
   new Talent(
-    'Damnation',
-    'spell_shadow_curseofsargeras',
-    ['Increases the damage done by your Curse of Agony by ', '% and increases the effect of your Curse of Weakness by ', '%.'],
+    "Damnation",
+    "spell_shadow_curseofsargeras",
+    [
+      "Increases the damage done by your Curse of Agony by ",
+      "% and increases the effect of your Curse of Weakness by ",
+      "%.",
+    ],
     3,
     [
       [2, 4, 6],
@@ -23,13 +27,38 @@ const tier3 = [
       isMoved: false,
     }
   ),
-  new Talent('Fel Concentration', 'spell_shadow_fingerofdeath', ['Gives you a ', '% chance to avoid interruption caused by damage while casting Drain Life, Drain Mana and Drain Soul spells.'], 5, [[14, 28, 42, 56, 70]]),
-  new Talent('Amplify Affliction', 'spell_shadow_contagion', ['Increases the effect of your next damage over time effect by 50%. Lasts 30 sec.'], 1, [[]], null, null, null, null, 'Instant', '3 min cooldown', null, {
-    isNew: true,
-    isChanged: false,
-    isMoved: false,
-  }),
+  new Talent(
+    "Fel Concentration",
+    "spell_shadow_fingerofdeath",
+    [
+      "Gives you a ",
+      "% chance to avoid interruption caused by damage while casting Drain Life, Drain Mana and Drain Soul spells.",
+    ],
+    5,
+    [[14, 28, 42, 56, 70]]
+  ),
+  new Talent(
+    "Amplify Affliction",
+    "spell_shadow_contagion",
+    [
+      "Increases the effect of your next damage over time or drain effect by 50%. Lasts 30 sec.",
+    ],
+    1,
+    [[]],
+    null,
+    null,
+    null,
+    null,
+    "Instant",
+    "3 min cooldown",
+    null,
+    {
+      isNew: true,
+      isChanged: false,
+      isMoved: false,
+    }
+  ),
   null,
-]
+];
 
-export default tier3
+export default tier3;
