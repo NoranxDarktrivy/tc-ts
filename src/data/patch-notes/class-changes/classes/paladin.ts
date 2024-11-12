1b9699c44d1c17fb9a71d27db09aae8d5a6d8c55
Djimovanberlo <djimovanberlo@gmail.com>
2024-11-12 23:25:52 +0100

    miin update

--- src/data/patch-notes/class-changes/classes/paladin.ts
+++ src/data/patch-notes/class-changes/classes/paladin.ts
@@ -1,13 +1,13 @@
 export default {
-  title: 'Paladin',
+  title: "Paladin",
   content: [
-    'New ability: Hand of Reckoning: Taunts the target to attack you. 10 sec cooldown. Learned at lvl 16.',
-    'Consecration is now a baseline ability. Learned at lvl 20.',
-    'Judgement no longer costs mana.',
-    'Exorcism and Holy Wrath can now be cast against any target, but always critical hit against Undead and Demon targets.',
-    'Seal of the Crusader reworked: Successful attacks increase your attack speed by 4%. Stacks up to 10 times and lasts 12 sec. Judgement effect remains the same.',
-    'Seal of Justice effect now stuns up to two nearby enemies.',
-    'Judgement of Justice now also caps the target’s movement speed at 100%.',
-    'Blessing of Might now also increases ranged attack power.',
+    "New ability: Hand of Reckoning: Taunts the target to attack you. 10 sec cooldown. Learned at lvl 16.",
+    "Consecration is now a baseline ability. Learned at lvl 20.",
+    "Judgement no longer costs mana.",
+    "Exorcism and Holy Wrath can now be cast against any target, but always critical hit against Undead and Demon targets.",
+    "Seal of the Crusader reworked: Reduces your melee damage by 30%, but your melee attacks affect an additional target. Judgement effect stays the same.",
+    "Seal of Justice effect now stuns up to two nearby enemies.",
+    "Judgement of Justice now also caps the target’s movement speed at 100%.",
+    "Blessing of Might now also increases ranged attack power.",
   ],
-}
+};
--- src/data/talents/paladin/retribution/tier7.ts
+++ src/data/talents/paladin/retribution/tier7.ts
@@ -1,17 +1,19 @@
-import { Talent } from '../../Classes'
+import { Talent } from "../../Classes";
 
 const tier7 = [
   null,
   new Talent(
-    'Crusader Strike',
-    'spell_holy_crusaderstrike',
-    ['An instant strike that causes 75% weapon damage as Holy and regenerates 5% of your maximum mana. The duration of all Judgement effects on the target is refreshed to 30 sec.'],
+    "Crusader Strike",
+    "spell_holy_crusaderstrike",
+    [
+      "An instant strike that causes 100% weapon damage as Holy and regenerates 5% of your maximum mana. The duration of all Judgement effects on the target is refreshed to 30 sec.",
+    ],
     1,
     [[]],
     null,
     null,
-    'Instant',
-    '6 sec cooldown',
+    "Instant",
+    "6 sec cooldown",
     null,
     null,
     null,
@@ -19,6 +21,6 @@ const tier7 = [
   ),
   null,
   null,
-]
+];
 
-export default tier7
+export default tier7;
