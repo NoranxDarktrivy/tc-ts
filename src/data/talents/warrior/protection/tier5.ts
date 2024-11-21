b08bf00a2738c0ff5a739e5894a207de3330eab5
Djimovanberlo <djimovanberlo@gmail.com>
2024-11-21 22:09:46 +0100

    update intervene

--- src/data/talents/warrior/protection/tier5.ts
+++ src/data/talents/warrior/protection/tier5.ts
@@ -1,14 +1,55 @@
-import { Talent } from '../../Classes'
+import { Talent } from "../../Classes";
 
 const tier5 = [
-  new Talent('Improved Shield Wall', 'ability_warrior_shieldwall', ['Increases the duration of your Shield Wall by ', ' sec.'], 2, [[3, 5]]),
-  new Talent('Concussion Blow', 'ability_thunderbolt', ['Stuns the target for 5 sec.'], 1, [[]], [{ x: null, y: null }], [{ x: 6, y: 1 }], '15 Rage', 'Melee Range', 'Instant cast', '45 sec cooldown', ['D2']),
-  new Talent('Improved Shield Bash', 'ability_warrior_shieldbash', ['Gives your Shield Bash a ', '% chance to silence the target for 2 sec.'], 2, [[50, 100]]),
-  new Talent('Spell Reflection', 'ability_warrior_shieldreflection', ['Raise your shield, reflecting the next spell cast on you.  Lasts 5 sec.'], 1, [[]], null, null, '25 Rage', null, 'Instant cast', '10 sec cooldown', null, {
-    isNew: true,
-    isChanged: false,
-    isMoved: false,
-  }),
-]
+  new Talent(
+    "Improved Shield Wall",
+    "ability_warrior_shieldwall",
+    ["Increases the duration of your Shield Wall by ", " sec."],
+    2,
+    [[3, 5]]
+  ),
+  new Talent(
+    "Concussion Blow",
+    "ability_thunderbolt",
+    ["Stuns the target for 5 sec."],
+    1,
+    [[]],
+    [{ x: null, y: null }],
+    [{ x: 6, y: 1 }],
+    "15 Rage",
+    "Melee Range",
+    "Instant cast",
+    "45 sec cooldown",
+    ["D2"]
+  ),
+  new Talent(
+    "Improved Shield Bash",
+    "ability_warrior_shieldbash",
+    ["Gives your Shield Bash a ", "% chance to silence the target for 2 sec."],
+    2,
+    [[50, 100]]
+  ),
+  new Talent(
+    "Intervene",
+    "ability_warrior_victoryrush",
+    [
+      "Run at high speed towards a party member, intercepting the next melee or ranged attack made against them.",
+    ],
+    1,
+    [[]],
+    null,
+    null,
+    "10 Rage",
+    "8 - 25 yd range",
+    "Instant cast",
+    "30 sec cooldown",
+    null,
+    {
+      isNew: true,
+      isChanged: false,
+      isMoved: false,
+    }
+  ),
+];
 
-export default tier5
+export default tier5;
